8e1909180041fba9b4cff3e1827c9b8ef275f182
jves0423@uni.sydney.edu.au <t13519jv@sfc.keio.ac.jp>
2019-05-03 01:17:05 +1000

    3May2019

--- app/controllers/analytics.server.controller.js
+++ app/controllers/analytics.server.controller.js
@@ -1,15 +1,149 @@
-/**
- * survey.server.controller.js
- *
- * This controller module exposes two methods:
- *  - showForm is used for displaying the form
- *  - showResult is used for showing the results
- * The methods are not mapped to URL yet
- */
-
 var express = require('express');
+var Revision = require('../models/revision');
 
+var nodisplay = "display: none;";
+var showdisplay = "display: block;";
 module.exports.showForm = function(req, res) {
-    products = req.app.locals.products;
-    res.render('index.ejs');
+    var errormsg = req.app.locals.errormsg;
+    var inputemail = req.app.locals.inputemail;
+    var inputfirstname = req.app.locals.inputfirstname;
+    var inputlastname = req.app.locals.inputlastname;
+    res.render('index.ejs', {errormsg: errormsg, inputemail:inputemail, inputfirstname:inputfirstname, inputlastname:inputlastname, registrationdisplay:nodisplay, logindisplay:showdisplay});
 };
+
+module.exports.showMain = function(req, res) {
+  console.log("ShowMAIN is ran");
+  var title = req.app.locals.title;
+  var allResult = {};
+  var count = 0;
+
+  //OVERALL Analytics
+  if(req.query.numberofarticle != null){var numberofarticle = parseInt(req.query.numberofarticle);}
+  else{var numberofarticle = 2;}
+  console.log(numberofarticle);
+  Revision.find2MostNumRev(numberofarticle, function(err, result){
+    if(err){console.log("ERROR");}
+    else{
+      allResult.mostNumRev1 = result[0]._id;
+      allResult.mostNumRev2 = result[1]._id;
+      count++;
+      parseAllResult(allResult, res, count, 'main.ejs');
+    }
+  });
+
+  Revision.find2LeastNumRev(parseInt(numberofarticle), function(err, result){
+    if(err){console.log("ERROR");}
+    else{
+      allResult.leastNumRev1 = result[0]._id;
+      allResult.leastNumRev2 = result[1]._id;
+      count++;
+      parseAllResult(allResult, res, count, 'main.ejs');
+    }
+  });
+
+  Revision.findLargestRegisteredUsers(function(err, result){
+    if(err){console.log("ERROR");}
+    else{
+      allResult.largestRegUserRev = result[0]._id;
+      count++;
+      parseAllResult(allResult, res, count, 'main.ejs');
+    }
+  });
+
+  Revision.overallBarChart(function(err, result){
+    if(err){console.log("ERROR");}
+    else{
+      allResult.overallBarChart = result;
+      count++;
+      parseAllResult(allResult, res, count, 'main.ejs');
+    }
+  });
+
+  Revision.overallPieChart(function(err, result){
+    if(err){console.log("ERROR");}
+    else{
+      allResult.overallPieChart = result;
+      count++;
+      parseAllResult(allResult, res, count, 'main.ejs');
+    }
+  });
+
+  Revision.overallRevAdminType(function(err, result){
+    if(err){console.log("ERROR");}
+    else{
+      allResult.overallRevAdminType = result;
+      count++;
+      parseAllResult(allResult, res, count, 'main.ejs');
+    }
+  });
+};
+
+function parseAllResult(allResult, res, count, viewfile){
+  if(count < 6){return;}
+  else{res.render(viewfile, {allResult: allResult});}
+}
+
+module.exports.getHighestRevision = function(req, res) {
+  var allResult = {};
+  allResult.mostNumRev = [];
+  allResult.leastNumRev = [];
+  var count = 0;
+  var numberofarticle = parseInt(req.query.numberofarticle);
+  Revision.find2MostNumRev(numberofarticle, function(err, result){
+    if(err){console.log("ERROR");}
+    else{
+      for(var i=0;i<numberofarticle;i++){
+        allResult.mostNumRev[i] = result[i]._id;
+      }
+      //res.render('highestRevision.ejs', {allResult: allResult});
+      count++;
+      parseHighestRevResult(allResult, res, count, 'highestRevision.ejs');
+    }
+  });
+  Revision.find2LeastNumRev(numberofarticle, function(err, result){
+    if(err){console.log("ERROR");}
+    else{
+      for(var i=0;i<numberofarticle;i++){
+        allResult.leastNumRev[i] = result[i]._id;
+      }
+      count++;
+      parseHighestRevResult(allResult, res, count, 'highestRevision.ejs');
+    }
+  });
+};
+
+function parseHighestRevResult(allResult, res, count, viewfile){
+  if(count < 2){return;}
+  else{res.render(viewfile, {allResult: allResult});}
+}
+
+/*
+module.exports.showResult = function(req, res) {
+
+  title = req.query.title;
+    console.log(title);
+
+    Revision.findTitleLatestRev(title, function(err,result){
+  		if (err){
+  			console.log("Cannot find " + title + ",s latest revision!")
+  		}else{
+  			// console.log(result)
+        if(result[0] != null){
+          //revision = result[0];
+          allResult.pop(result[0]);
+          //renderMain(res, revision);
+
+          $.get('/main/getResult', title, function(res,req){
+
+          });
+    			//res.render('main.ejs',{title: title, revision:revision})
+        }
+        else{
+          console.log("Cannot find " + title + "'s latest revision!")
+        }
+
+  		}
+  	})
+
+
+};*/
--- app/routes/analytics.server.routes.js
+++ app/routes/analytics.server.routes.js
@@ -3,5 +3,10 @@ var controller = require('../controllers/analytics.server.controller')
 var router = express.Router();
 
 router.get('/', controller.showForm);
-//router.post('/survey', controller.showResult);
+
+//change back to this later
+router.post('/main', controller.showMain);
+router.get('/main', controller.showMain);
+router.get('/main/getHighestRevision', controller.getHighestRevision);
+//router.get('/main/getResult', controller.showResult);
 module.exports = router;
